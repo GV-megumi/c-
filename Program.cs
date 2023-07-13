a5c6a3fb872b2f2ea065775084e048392c8c6af3
GV <136101185+GV-megumi@users.noreply.github.com>
2023-07-13 23:22:43 +0800

    11

--- Program.cs
+++ Program.cs
@@ -5,9 +5,22 @@ namespace asss
 {
          class program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("helloqqqqq,world");
+            Console.WriteLine("aaaaaa");
+
+            int[] num={1,2,3};
+            int a=1,b=2;
+            //Console.Write("bbbbbbb{2} and {1}",num); 不可用
+            //Console.WriteLine("bbbbbbb{2} and {3}\n",1,2,3);  3:抛出异常
+            Console.WriteLine("bbbbbbb{2} and {1}",1,2,3);
+            //格式化数字字符串
+            Console.WriteLine($"{a:C} and {b:f3}");
+
+
+
+            
 
         }
     }
